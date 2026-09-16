d5bf03ac001b01bbfabba585b3ab027c4885b753
ThomasLee <sardonyx816@gmail.com>
2026-09-16 11:00:56 +0800

    fix: 補上 MainActivity.kt 遺漏之 android.util.Log import

--- app/src/main/java/org/rotary/goodsassistant/MainActivity.kt
+++ app/src/main/java/org/rotary/goodsassistant/MainActivity.kt
@@ -5,6 +5,7 @@ import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.webkit.CookieManager
 import android.webkit.JavascriptInterface
